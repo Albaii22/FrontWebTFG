d9a3e6de642038bb5242083040a01b0d030f4a2e
albaii22 <korusshow@gmail.com>
2024-05-17 17:55:34 +0200

    feat: creation of the html and css to login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/modules/login/login.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  
+}
+ 
