85ffd064b92864981bd8aef882005f86f4604cf1
albaii22 <korusshow@gmail.com>
2024-05-17 18:43:50 +0200

    feat: adding logic to login and get token

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,16 +1,44 @@
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { LoginI } from '../../interfaces/login.interface';
+import { AuthService } from '../../services/authService/auth.service';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule , FormsModule , HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  LoginForm = {
+    username: '',
+    password: ''
+  }
+
+  constructor(private authService: AuthService , private router: Router) {}
+
+  Login(form: LoginI): void {
+    if (this.LoginForm.username !== '' || this.LoginForm.password !== '') {
+      this.authService.Login(form).subscribe({
+        next: (data) => {
+          console.log('Succesfull Login!!' , data);
+          this.router.navigate([]);
+        },
+        error: (err) => {
+          console.log('Ivalid data', err);
+        }
+      });
+    } else {
+      console.log('No data on username and password');
+    }
+  }
+  
   
 }
  
