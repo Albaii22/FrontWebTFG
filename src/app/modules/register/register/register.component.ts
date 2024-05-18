bee4fa9789692867efb1759946b86960e0cc827c
albaii22 <korusshow@gmail.com>
2024-05-18 23:09:11 +0200

    feat: creation of the logic to register a user

--- src/app/modules/register/register/register.component.ts
+++ src/app/modules/register/register/register.component.ts
@@ -1,28 +1,59 @@
 import { Component } from '@angular/core';
+import { RegisterI } from '../../../interfaces/register.interface';
+import { AuthService } from '../../../services/authService/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
 
-  RegisterForm= {
+  RegisterForm = {
     username: '',
     email: '',
     password: ''
   }
 
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  Register(form: any): void {
-    if (this.RegisterForm.username !== '' || this.RegisterForm.email !== '' || this.RegisterForm.password !== '') {
-      console.log(this.RegisterForm);
+  Register(form: RegisterI): void {
+    if (this.RegisterForm.username !== '' || this.RegisterForm.password !== '') {
+      this.authService.Register(form).subscribe({
+        next: (data) => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Successful register!',
+            text: 'You have registered successfully.',
+          });
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Invalid data',
+            text: 'There was an error during the registration process.',
+          });
+        }
+      });
     } else {
-      console.log('No data on username and password');
+      Swal.fire({
+        icon: 'warning',
+        title: 'Incomplete data',
+        text: 'Please fill out all required fields.',
+      });
     }
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
   }
 
 }
