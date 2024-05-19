6af08423067bf712ab13e0c305fde96f6aad6532
albaii22 <usr3584@salesianos-lacuesta.net>
2024-05-19 12:28:13 +0100

    feat: creation of the publications service

---
+++ src/app/services/publications/publications.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { PublicationI } from '../../interfaces/publications.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PublicationsService {
+
+  private urlPublications = 'http://localhost:8082/api/publications';
+
+  constructor(private http: HttpClient) {}
+
+  getAllPublications(): Observable<PublicationI[]> {
+    return this.http.get<PublicationI[]>(this.urlPublications);
+  }
+
+  getPublicationById(id: number): Observable<PublicationI> {
+    return this.http.get<PublicationI>(`${this.urlPublications}/${id}`);
+  }
+
+  createPublication(publication: PublicationI, userId: number): Observable<PublicationI> {
+    return this.http.post<PublicationI>(`${this.urlPublications}/${userId}`, publication);
+  }
+
+  updatePublication(id: number, publication: PublicationI): Observable<PublicationI> {
+    return this.http.put<PublicationI>(`${this.urlPublications}/${id}`, publication);
+  }
+
+  deletePublication(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.urlPublications}/${id}`);
+  }
+}
